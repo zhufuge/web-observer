2a3b05d29f5f2789b777c172642e81baaebaf383
6ml <344655454@qq.com>
2017-06-11 23:34:08 +0800

    Finish URL getting and storage

--- extension/background.js
+++ extension/background.js
@@ -1,7 +1,43 @@
-function openPage() {
-  browser.tabs.create({
-    url: "index.html"
-  });
-}
+(() => {
+  // listen browser_action
+  function openPage() {
+    browser.tabs.create({
+      url: "index.html"
+    });
+  }
 
-browser.browserAction.onClicked.addListener(openPage);
+  browser.browserAction.onClicked.addListener(openPage);
+})();
+
+(() => {
+  // url storage
+
+  const HOST_REGEXP = /:\/\/([^\/]+)\//;
+  const onError = e => console.log(e);
+
+  function handleUpdated(tabId, changeInfo, tabInfo) {
+    if (changeInfo.url) {
+      const curHost = HOST_REGEXP.exec(changeInfo.url)[1];
+      console.log(curHost);
+
+      const getting = browser.storage.local.get(curHost);
+      console.log(getting);
+      getting
+        .catch(onError)
+        .then(item => {
+          const times = item[curHost] || 0;
+          return browser.storage.local.set({[curHost]: times + 1});
+        })
+        .catch(onError);
+    }
+  }
+
+  browser.tabs.onUpdated.addListener(handleUpdated);
+
+  function handleRemoved(tabId, removeInfo) {
+    const curTime = new Date();
+    console.log(curTime);
+  }
+
+  browser.tabs.onRemoved.addListener(handleRemoved);
+})();
--- extension/index.js
+++ extension/index.js
@@ -1,1 +1,4 @@
-console.log('wa haahahahah');
+const getting = browser.storage.local.get();
+getting.then(item => {
+  console.log(item);
+}).catch(e => console.log(e));
