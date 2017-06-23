856262e2a19d1b0a97378c79756698351a42972f
6ml <344655454@qq.com>
2017-06-23 22:48:56 +0800

    Add time chart

--- app/common/time-draw.js
+++ app/common/time-draw.js
@@ -10,16 +10,113 @@ function extractDate(item) {
   return all;
 }
 
-function drawChart(canvas) {
+function time2Number(time) {
+  const sub = (a, b) => time.substr(a, b);
+  return parseInt(sub(0, 2)) * 60 * 60 +
+    parseInt(sub(3, 2)) * 60 +
+    parseInt(sub(6, 2));
+}
+
+function toLineSet(array) {
+  const DAY = 60 * 60 * 24 - 1;
+  const data = [];
+
+  let last = 0, temp = 0;
+  for (let time of array) {
+    if (!data) {
+      if (time.length === 1) {
+        last = time2Number(time[0]);
+        data.push(last);
+      } else {
+        temp = time2Number(time[0]);
+        last = time2Number(time[1]);
+        data.push(temp);
+        data.push(last - temp);
+      }
+    } else {
+      for (let i = 0; i < 2; i++) {
+        temp = last;
+        last = time2Number(time[i]);
+        data.push(last - temp);
+      }
+    }
+  }
+
+  data.push(DAY - last);
+
+  return data;
+}
+
+const NOTUSE = '未启用时间',
+      USE = '启用时间',
+      GRAY = 'rgba(0, 0, 0, 0.1)',
+      ACTIVE = 'rgba(255, 99, 132, 0.8)';
+
+function getColors(length, isStart0) {
+  const colors = [];
+  for (let i = 0; i < length; i++) {
+    if (i % 2 === 0) {
+      colors.push(isStart0 ? ACTIVE : GRAY);
+    } else {
+      colors.push(isStart0 ? GRAY : ACTIVE);
+    }
+  }
+
+  return colors;
+}
+
+function getLabels(length, isStart0) {
+  const labels = [];
+  for (let i = 0; i < length; i++) {
+    if (i % 2 === 0) {
+      labels.push(isStart0 ? USE : NOTUSE);
+    } else {
+      labels.push(isStart0 ? NOTUSE : USE);
+    }
+  }
+
+  return labels;
+}
+
+function drawChart(canvas, date) {
   return function(item) {
-    const all = extractDate(item);
+    const ctx = canvas.getContext('2d');
+    let myChart;
+    if (Object.keys(item).length !== 0) {
+      const data = toLineSet(item[date]),
+            isStart0 = item[date][0].length === 1,
+            labels = getLabels(data.length,isStart0),
+            colors = getColors(data.length, isStart0);
+      myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+          labels,
+          datasets: [{
+            label: 'times',
+            data,
+            backgroundColor: colors,
+          }]
+        }
+      });
+    } else {
+      myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+          labels: ['未启用时间'],
+          datasets: [{
+            label: 'times',
+            data: [1],
+          }]
+        }
+      });
+    }
   };
 }
 
-function timeDraw(canvas) {
+function timeDraw(canvas, date) {
   if (browser) {
-    browser.storage.local.get()
-      .then(drawChart(canvas))
+    browser.storage.local.get(date)
+      .then(drawChart(canvas, date))
       .catch(e => console.log(e));
   }
 }
--- app/components/MainContent.js
+++ app/components/MainContent.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {DatePicker} from 'antd';
+import { DatePicker, Card } from 'antd';
 import moment from 'moment';
 
-import timesDraw from '../common/times-draw';
+import freqDraw from '../common/freq-draw';
 import timeDraw from '../common/time-draw';
 
 export default
@@ -10,40 +10,47 @@ class MainContent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      timeDateSelect: moment().format('YYYY/M/D')
+      date: moment().format('YYYY/M/D')
     };
-    this.timeDateChange = this.timeDateChange.bind(this);
   }
-  timeDateChange(value, dateString) {
-    this.setState({timeDateSelect: dateString});
-  }
-  renderTimes() {
+
+  renderFreq() {
     return (
       <div style={styles.container}>
-        <canvas ref={timesDraw}
-                width="200" height="100"/>
+        <canvas ref={freqDraw} width="200" height="100"/>
       </div>
     );
   }
   renderTime() {
+    const datePicker = (
+      <div>
+        <span>选择查询的日期&nbsp;:&nbsp;</span>
+        <DatePicker
+          allowClear={false}
+          size="large"
+          value={moment(this.state.date)}
+          format="YYYY/M/D"
+          onChange={(v, date) => date ? this.setState({date}) : 0}
+          disabledDate={cur => moment() <= cur}
+          />
+      </div>
+    );
     return (
       <div style={styles.container}>
-        <DatePicker
-          value={moment(this.state.timeDateSelect)}
-          format="YYYY/M/D"
-          onChange={this.timeDateChange} />
-        <div>{this.state.timeDateSelect}</div>
-        <canvas ref={timeDraw}
-                width="200" height="100"/>
+        <Card title={datePicker}>
+          <canvas
+            ref={ref => timeDraw(ref, this.state.date)}
+            width="200" height="80"/>
+        </Card>
       </div>
     );
   }
 
   render() {
     switch(this.props.item) {
-    case '1': return this.renderTimes();
+    case '1': return this.renderFreq();
     case '2': return this.renderTime();
-    default: return this.renderTimes();
+    default: return this.renderFreq();
     }
   }
 }
@@ -53,5 +60,6 @@ const styles = {
     margin: 30,
     background: '#fff',
     padding: 30,
-  }
+    borderRadius: 3,
+  },
 };
